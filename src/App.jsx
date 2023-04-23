80c5ba02a2ebdc1626cd1d576730c3fcfce9f2f1
Pedro Sousa <PedroGouveiaSousa@Gmail.com>
2023-04-24 00:40:12 +0100

    starting ...

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+    return (
+        <section class="section">
+            <div class="container">
+                <h1>App Component</h1>
+            </div>
+        </section>
+    );
+}
+
+export default App;
